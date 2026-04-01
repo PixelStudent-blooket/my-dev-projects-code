a93d908a97c17b34ecf5d4dd389785b55d2fa266
PixelStudent-blooket <ericgutiantian@hotmail.com>
2026-04-01 16:29:33 -0400

    Create issues.py

---
+++ issues.py
@@ -0,0 +1,3 @@
+there may be many issues due to python mixing tabs which will make it not work  even if the code looks perfectly fine 
+
+to fix this open note pad then paste the engine code in then save as then call it engine.py open the file then after  paste the code
