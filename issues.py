39d2aa56c383043a27c1f5f81b22edb09bca3b18
PixelStudent-blooket <ericgutiantian@hotmail.com>
2026-04-01 21:28:23 -0400

    Update issues.py

--- issues.py
+++ issues.py
@@ -1,3 +1,1 @@
-there may be many issues due to python mixing tabs which will make it not work  even if the code looks perfectly fine 
-
-to fix this open note pad then paste the engine code in then save as then call it engine.py open the file then after  paste the code
+itis used on pycharm if there are any issues please report .
